cde74a453e344d4c369cd5cd6f63c8976225d7bf
Waggler <khdavis4@gmail.com>
2021-03-09 16:29:02 -0500

    Commit

---
+++ GithubTesticles/Assets/Nav.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Charlie is a dirty dirty boy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
